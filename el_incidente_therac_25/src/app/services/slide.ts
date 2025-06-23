c5d999e8d44cd543c4df26260bfbeb05a08c8ccc
Nicolás Butterfield <nicobutter@gmail.com>
2025-06-23 09:47:46 -0300

    se agregaron slides

--- el_incidente_therac_25/src/app/services/slide.ts
+++ el_incidente_therac_25/src/app/services/slide.ts
@@ -15,13 +15,16 @@ export class SlideService {
         'Estos accidentes pusieron en duda la fiabilidad del control por software de sistemas de seguridad crítica, convirtiéndose en caso de estudio en la informática médica y en la ingeniería de software',
         //'Párrafo'
       ],
-      images: ['assets/img/diapositiva_1_1.png']
+      images: ['assets/img/diapositiva_1_1.png'],
+      footerText:'Diapositiva_1'
     },
     {
       title: 'Origen y Contexto',
       text: 'Reemplazo de hardware por softwar. Aprobación rápida por equivalencia',
       paragraphs:['El Therac‑25, lanzado en 1983, fue el primer modelo de AECL que suprimió prácticamente todos los bloqueos físicos (interlocks) utilizados en sus predecesores (Therac‑6/20), confiando todo en controles electrónicos y software', 
-                  'Este cambio fue acreditado por la FDA mediante un “equivalente pre‑mercado” ya que se basaba en software y hardware previamente existentes, lo cual evitó pruebas rigurosas – aunque internamente la arquitectura había cambiado drásticamente '],
+                  'Este cambio fue acreditado por la FDA mediante un “equivalente pre‑mercado” ya que se basaba en software y hardware previamente existentes, lo cual evitó pruebas rigurosas – aunque internamente la arquitectura había cambiado drásticamente '
+      ],
+      images: ['assets/img/therac_25_origenes.png']
     },
     {
       title: '¿Cómo funcionaba el Therac‑25?',
@@ -33,11 +36,17 @@ export class SlideService {
     },
     {
       title: 'Errores de Software',
-      text: 'Aquí va el texto sobre los bugs críticos en el software.'
+      text: 'El operador y la terminal',
+      paragraphs:['El operador ingresaba los datos del tratamiento a través de un teclado y una interfaz tipo terminal verde monocroma. Era una especie de menú con texto donde se elegía el tipo de haz, la dosis, la posición del paciente, etc. Se podía corregir rápidamente un dato mal ingresado usando la tecla "↑" (flecha arriba) para editar.'
+      , 'Si el operador era rápido y modificaba un parámetro (por ejemplo, de "haz de electrones" a "haz de rayos X") usando la flecha ↑, la interfaz mostraba el cambio, pero el estado interno del software no actualizaba correctamente. Esto generaba un "race condition" (condición de carrera): dos procesos ejecutándose casi al mismo tiempo, donde uno no alcanzaba a actualizar al otro.', 
+    'Resultado: el sistema pensaba que debía usar un haz de baja energía (electrones), pero no activaba el dispersor para rayos X, dejando pasar una dosis 100 veces más potente directamente al cuerpo del paciente.'],
+      images: ['assets/img/Terminal_DEC _VT-100.png']
     },
     {
-      title: 'Incidentes Reportados',
-      text: 'Aquí va el texto sobre los accidentes y sus consecuencias.'
+      title: 'Mensaje típico que veía el operador:',
+      text: 'En la pantalla de la terminial de operador se disparaban mensajes de error inentendibles por el usuario y que además no se detallaban en la documentación.',
+      paragraphs: ['MALFUNCTION 54 \n NO DOSE DELIVERED', 'Traducción humana: \n Parece que no se administró la dosis. Vuelva a intentarlo. \n Pero en realidad… ¡el paciente ya había recibido una descarga letal de radiación!'],
+      images: ['assets/img/mensaje_de_error.png']
     },
     {
       title: 'Causas de los Fallos',
